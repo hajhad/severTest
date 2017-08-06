70550dd27fb63294d6872f62af3fe988cd0ba4b4
hajhad <306144403@qq.com>
2017-08-06 15:45:53 +0800

    创建服务器

---
+++ ServerTest/ServerTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerTest
+{
+    class Program
+    {
+        static  Socket serverSocket = null;
+        static void Main(string[] args)
+        {
+
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 8899);
+            serverSocket.Bind(ipEndPoint);
+            serverSocket.Listen(10);
+
+            Thread thread = new Thread(listenClientConnect);
+            thread.Start();
+
+        }
+
+
+      private static void listenClientConnect()
+        {
+            Socket clientSocket = serverSocket.Accept();
+            clientSocket.Send(Encoding.Default.GetBytes("服务器告诉你连接成功"));
+            Thread reeThread = new Thread(reeThreadClientMessage);
+            reeThread.Start();
+        }
+
+        static void reeThreadClientMessage(object clientSocket)
+        {
+            Socket socket = clientSocket as Socket;
+
+            byte[] buffer = new byte[1024];
+
+          int length =  socket.Receive(buffer);
+
+            Console.WriteLine(Encoding.Default.GetString(buffer));
+        }
+    }
+
+   
+
+  
+}
